2fef27a2b41e168041df3235ea30d861c1873515
Alexandr Soldatenkov <amsdevelops@gmail.com>
2020-09-14 23:36:43 +0300

    Added CardView with posters + buttons logic

--- app/src/main/java/com/amsdevelops/filmssearch/MainActivity.kt
+++ app/src/main/java/com/amsdevelops/filmssearch/MainActivity.kt
@@ -2,10 +2,32 @@ package com.amsdevelops.filmssearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initButtons()
+    }
+
+    private fun initButtons() {
+        button_menu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        button_fav.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        button_watch_later.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        button_selections.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        button_settings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
