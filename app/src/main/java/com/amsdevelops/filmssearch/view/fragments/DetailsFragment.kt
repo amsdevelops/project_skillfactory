d03bd422167b52b9267a44bbd79d44d5e15e81f3
--replace <mail@yourmail.ru>
2021-01-18 10:17:35 +0300

    Module_45 done

--- app/src/main/java/com/amsdevelops/filmssearch/view/fragments/DetailsFragment.kt
+++ app/src/main/java/com/amsdevelops/filmssearch/view/fragments/DetailsFragment.kt
@@ -1,20 +1,36 @@
 package com.amsdevelops.filmssearch.view.fragments
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.amsdevelops.filmssearch.data.entity.Film
 import com.amsdevelops.filmssearch.R
 import com.amsdevelops.filmssearch.data.ApiConstants
 import com.amsdevelops.filmssearch.databinding.FragmentDetailsBinding
+import com.amsdevelops.filmssearch.viewmodel.DetailsFragmentViewModel
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.*
 
 class DetailsFragment : Fragment() {
     private lateinit var film: Film
     private lateinit var binding: FragmentDetailsBinding
+    private val viewModel: DetailsFragmentViewModel by viewModels()
+    private val scope = CoroutineScope(Dispatchers.IO)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,6 +69,15 @@ class DetailsFragment : Fragment() {
             //Запускаем наше активити
             startActivity(Intent.createChooser(intent, "Share To:"))
         }
+
+        binding.detailsFabDownloadWp.setOnClickListener {
+            performAsyncLoadOfPoster()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
     }
 
     private fun setFilmsDetails() {
@@ -73,5 +98,106 @@ class DetailsFragment : Fragment() {
             if (film.isInFavorites) R.drawable.ic_baseline_favorite_24
             else R.drawable.ic_baseline_favorite_border_24
         )
+    }
+
+    private fun performAsyncLoadOfPoster() {
+        //Проверяем есть ли разрешение
+        if (!checkPermission()) {
+            //Если нет, то запрашиваем и выходим из метода
+            requestPermission()
+            return
+        }
+        //Создаем родительский скоуп с диспатчером Main потока, так как будет взаимодействовать с UI
+        MainScope().launch {
+            //Включаем прогресс бар
+            binding.progressBar.isVisible = true
+            //Создаем через async так как нам нужен результат от работы, то есть Bitmap
+            val job = scope.async {
+                viewModel.loadWallpaper(ApiConstants.IMAGES_URL + "original" + film.poster)
+            }
+            //Сохраняем в галерею, как только файл загрузится
+            saveToGallery(job.await())
+            //Выводим снекбар с кнопкой перейти в галерею
+            Snackbar.make(
+                binding.root,
+                R.string.downloaded_to_gallery,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.open) {
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_VIEW
+                    intent.type = "image/*"
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+                .show()
+
+            //Отключаем прогресс бар
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    //Узнаем, было ли получено разрешение ранее
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+    //Запрашиваем разрешение
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+    }
+
+    private fun saveToGallery(bitmap: Bitmap) {
+        //Проверяем версию системы
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //Создаем объект для передачи данных
+            val contentValues = ContentValues().apply {
+                //Составляем информацию для файла(имя, тип, дата создания, куда сохранять и т.д.)
+                put(MediaStore.Images.Media.TITLE, film.title.handleSingleQuote())
+                put(
+                    MediaStore.Images.Media.DISPLAY_NAME,
+                    film.title.handleSingleQuote()
+                )
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(
+                    MediaStore.Images.Media.DATE_ADDED,
+                    System.currentTimeMillis() / 1000
+                )
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/FilmsSearchApp")
+            }
+            //Получаем ссылку на объект Content resolver, которые помогает передвать информацию из приложения во вне
+            val contentResolver = requireActivity().contentResolver
+            val uri = contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+            //Открываем канал для записи на диск
+            val outputStream = contentResolver.openOutputStream(uri!!)
+            //Передаем нашу картинку, может сделать компрессию
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            //Закрываем поток
+            outputStream?.close()
+        } else {
+            //Тоже, но для более старых версий ОС
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.insertImage(
+                requireActivity().contentResolver,
+                bitmap,
+                film.title.handleSingleQuote(),
+                film.description.handleSingleQuote()
+            )
+        }
+    }
+
+    private fun String.handleSingleQuote(): String {
+        return this.replace("'", "")
     }
 }
---
+++ app/src/main/java/com/amsdevelops/filmssearch/viewmodel/DetailsFragmentViewModel.kt
@@ -0,0 +1,19 @@
+package com.amsdevelops.filmssearch.viewmodel
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.lifecycle.ViewModel
+import java.net.URL
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+
+class DetailsFragmentViewModel : ViewModel() {
+    suspend fun loadWallpaper(url: String): Bitmap {
+        return suspendCoroutine {
+            val url = URL(url)
+            val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+            it.resume(bitmap)
+        }
+    }
+}
